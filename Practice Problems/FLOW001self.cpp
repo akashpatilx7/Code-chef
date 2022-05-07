7fd29f26dc5ed3e6715e5081843121474cf45ce5
akashpatilx7 <101398263+akashpatilx7@users.noreply.github.com>
2022-05-08 01:05:34 +0530

    Create FLOW001self.cpp

---
+++ Practice Problems/FLOW001self.cpp
@@ -0,0 +1,22 @@
+// Shivam is the youngest programmer in the world, he is just 12 years old. Shivam is learning programming and today he is writing his first program.The task is very simple: given two integers A and B, write a program to add these two numbers and output it.
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+    cin>>T;
+
+    while(T--)
+    {
+        int A,B;
+        cin>>A>>B;
+
+        cout<<A+B<<endl;
+
+    }
+	return 0;
+}
+
