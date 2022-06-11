9e91447a59437657fa2f6bb60118dae0247ba45d
akashpatilx7 <101398263+akashpatilx7@users.noreply.github.com>
2022-06-11 10:30:23 +0530

    Create DOUBTSUPPORT.cpp

---
+++ Contest Problems/STARTERS 40/DOUBTSUPPORT.cpp
@@ -0,0 +1,20 @@
+/* Doubt Support Availability
+Problem Code: DOUBTSUPPORT
+Contest Code: START40 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int D;
+	cin>>D;
+	
+	if(D<=1600){
+	    cout<<"Yes"<<"\n";
+	}
+	
+	else{
+	    cout<<"No"<<"\n";
+	}
+	return 0;
+}
