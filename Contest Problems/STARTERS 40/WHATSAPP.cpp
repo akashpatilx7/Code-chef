0bd14deab4f6e6ab031cde61b57b2c8e10d0bac8
akashpatilx7 <101398263+akashpatilx7@users.noreply.github.com>
2022-06-11 10:43:15 +0530

    Create WHATSAPP.cpp

---
+++ Contest Problems/STARTERS 40/WHATSAPP.cpp
@@ -0,0 +1,19 @@
+/* Whatsapp Messages
+Problem Code: WHATSAPP
+Contest Code: START40 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	
+	while(T--){
+	    int X,Y,Z;
+	    cin>>X>>Y>>Z;
+	    
+	    cout<<(X-Y)*Z<<"\n";
+	}
+	return 0;
+}
