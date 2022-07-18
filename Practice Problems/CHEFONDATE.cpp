2cf310ab8b97d1ad6314b3b70be49e2d36b47e70
akashpatilx7 <101398263+akashpatilx7@users.noreply.github.com>
2022-07-18 18:59:01 +0530

    Create CHEFONDATE.cpp

---
+++ Practice Problems/CHEFONDATE.cpp
@@ -0,0 +1,22 @@
+/* Chef On Date
+Problem Code: CHEFONDATE
+Contest Code: START47 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    
+    while(T--){
+        int X,Y;
+        cin>>X>>Y;
+        if(X>=Y){
+            cout<<"YES"<<"\n";
+        }
+        else
+        cout<<"NO"<<"\n";
+    }
+	return 0;
+}
