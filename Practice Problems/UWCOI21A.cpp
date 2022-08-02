e1e3094dab0aeeb1dc11b51acbe2e9ed0a0bc1ec
akashpatilx7 <101398263+akashpatilx7@users.noreply.github.com>
2022-08-02 18:00:12 +0530

    Create UWCOI21A.cpp

---
+++ Practice Problems/UWCOI21A.cpp
@@ -0,0 +1,20 @@
+/* Hidden Numbers
+Problem Code: UWCOI21A
+Contest Code: UWCOI21 */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    
+    while(T--){
+        int N;
+        cin>>N;
+        
+        cout<<1<<" "<<N<<'\n';
+    }
+	return 0;
+}
