f549bccef99b99a57c43454908b65902b8353ef5
akashpatilx7 <101398263+akashpatilx7@users.noreply.github.com>
2022-08-07 16:25:23 +0530

    Create CHFINTRO.cpp

---
+++ Practice Problems/CHFINTRO.cpp
@@ -0,0 +1,34 @@
+/* Chef and Interactive Contests
+Problem Code: CHFINTRO
+Contest Code: DEC18 */ 
+
+#include<bits/stdc++.h>
+#define ll long long
+#define llmax LLONG_MAX
+#define llmin LLONG_MIN
+#define imax INT_MAX
+#define imin INT_MIN
+using namespace std;
+
+/*void solution(){
+}*/
+
+int main() {
+    int N,r;
+    cin>>N>>r;
+    
+    while(N--){
+        // solution();
+        int R;
+        cin>>R;
+        
+        if(R<r){
+            cout<<"Bad boi"<<'\n';
+        }
+        
+        else{
+            cout<<"Good boi"<<'\n';
+        }
+    }
+	return 0;
+}
